1655af27c3ff729724485875bc3b77fa4eb6f34a
akspaso <akspas0@gmail.com>
2022-07-23 16:58:45 +0530

    added request mapping

--- src/main/java/com/aks/DemoApplication.java
+++ src/main/java/com/aks/DemoApplication.java
@@ -2,10 +2,18 @@ package com.aks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+
+@RestController
 public class DemoApplication {
 
+	@RequestMapping("welcome")
+	public String welcome(){
+		return "welcome to github actions.";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
